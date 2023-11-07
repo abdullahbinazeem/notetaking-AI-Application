0659f7c18718203172ad43e22283d00d58f9023b
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2023-11-06 21:37:52 -0800

    created landing page for ai assistant

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
+import TypewriterTitle from "@/components/TypewriterTitle";
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <div className="text-red-600">asd</div>
+    <div className=" bg-gradient-to-r min-h-screen from-rose-100 to-teal-100 grainy">
+      <div className="h-screen w-screen grid place-content-center">
+        <h1 className="font-semibold text-7xl text-center">
+          AI <span className="text-green-600 font-bold">note taking</span>{" "}
+          assistant.
+        </h1>
+        <div className="mt-4">
+          <h2 className="font-semibold text-3xl text-center text-slate-700">
+            <TypewriterTitle />
+          </h2>
+        </div>
+        <div className="mt-8"></div>
+
+        <div className="flex justify-center">
+          <Link href="/dashboard">
+            <Button className="bg-green-600">Get Started</Button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
