10bbb1b85215209ad042ff83052b5ac398d14ec9
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2023-12-02 01:01:28 -0800

    lowered edge cost runtimes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,9 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/edgestore/init"],
 });
 
 export const config = {
-  matcher: [
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    "/",
-    "/(api|trpc)(.*)",
-    "/api/edgestore/init",
-  ],
+  matcher: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/api/edgestore/init"],
 };
