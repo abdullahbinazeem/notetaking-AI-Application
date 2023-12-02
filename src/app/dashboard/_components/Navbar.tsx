255e8038be56abcaf8c7a05b7d91c6755963cf48
Powdee123 <66191086+Powdee123@users.noreply.github.com>
2023-12-01 16:46:40 -0800

    Debounce change

--- src/app/dashboard/_components/Navbar.tsx
+++ src/app/dashboard/_components/Navbar.tsx
@@ -10,7 +10,10 @@ const Navbar: React.FC<NavbarProps> = ({ nav }) => {
   return (
     <ul className="flex justify-center gap-x-12 text-xl">
       {nav.map((item) => (
-        <li className="border-b-2 border-green-600 px-2 cursor-pointer hover:border-b-2 hover:border-green-100">
+        <li
+          key={item.id}
+          className="border-b-2 border-green-600 px-2 cursor-pointer hover:border-b-2 hover:border-green-100"
+        >
           {item.name}
         </li>
       ))}
